d7e3431bbd6e80e41fc3ad6028093834da00da89
RajkumarMondal-VaraTech <rajkumar.mondal@varatechnology.com>
2023-12-27 16:57:01 +0530

    Frontend is now dockirized

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This repository is used to develop sastamedicine project
+
+Node Version: 20.10.0
---
+++ apps/frontend/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "======================Frontend-Start===================="
+
+yarn dev
